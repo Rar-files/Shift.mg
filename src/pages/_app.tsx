52ede2fe042d2842550cdccd23c8b90fa33bf636
rar-files <dwidlak@outlook.com>
2022-03-16 00:49:09 +0100

    ConvertAppToNextJs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import type { AppProps } from 'next/app'
+
+const App: FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+)
+
+export default App;
