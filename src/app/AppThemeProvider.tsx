44db9164cb1478d8f7d7ea689ef4173aa286a03a
rar-files <dwidlak@outlook.com>
2022-03-31 00:23:08 +0200

    finalizeKIW45Tasks

--- src/app/AppThemeProvider.tsx
+++ src/app/AppThemeProvider.tsx
@@ -1,19 +1,21 @@
 import useDarkMode from "use-dark-mode";
 import { ThemeProvider } from "styled-components";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 import { darkTheme, lightTheme } from "../styles/Theme";
+import {Reset} from "styled-reset";
 
 type Props = {
     children?: React.ReactNode;
 };
 
 const AppThemeProvider : FC<Props> = ({ children }) => {
-    const darkMode = useDarkMode(true)
-    const theme = darkMode.value ? darkTheme : lightTheme
+    const darkMode = useDarkMode()
+    const theme = darkMode.value ? darkTheme : lightTheme;
     
     return (
         <ThemeProvider theme={theme}>
+            <Reset/>
             {children}
         </ThemeProvider>
     );
--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react";
 import styled from "styled-components";
 import useDarkMode from "use-dark-mode";
+import UserPanel from "./UserPanel";
+
+const Menu = styled.div`
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
+    padding: 10px;
+    margin-bottom: 10px;
+`;
 
 const MenuBar: FC = () => {
     return (
-        <>
-        </>
+        <Menu>
+            <UserPanel/>
+        </Menu>
     )
 };
   
